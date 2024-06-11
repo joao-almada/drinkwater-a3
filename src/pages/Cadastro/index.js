d39e5b71628881fb7e629b54fad43acda48119aa
João Almada <jvcalmada@hotmail.com>
2024-06-10 22:40:20 -0300

    cadastro

---
+++ src/pages/Cadastro/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import styles from './Cadastro.module.css'
+import AppRoutes from './../../routes'
+import { Link, useNavigate } from 'react-router-dom'
+import useAuth from "../../hooks/useAuth";
+
+const Cadastro = () => {
+    const [email, setEmail] = useState("");
+    const [emailConf, setEmailConf] = useState("");
+    const [senha, setSenha] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const { signup } = useAuth();
+
+    const handleSignup = () => {
+      if (!email | !emailConf | !senha) {
+        setError("Preencha todos os campos");
+        return;
+      } else if (email !== emailConf) {
+        setError("Os e-mails não são iguais");
+        return;
+      }
+  
+      const res = signup(email, senha);
+  
+      if (res) {
+        setError(res);
+        return;
+      }
+  
+      alert("Usuário cadatrado com sucesso!");
+      navigate("/");
+    };  
+
+    return (
+        <section>
+            <div className={styles.container}>
+            <form>
+                <h1>Meu cadastro</h1>
+                <div className={styles.input_field}>
+                    <input type="email" placeholder="E-mail" value={email} onChange={(e) => [setEmail(e.target.value), setError("")]}></input>
+                </div>
+                <div className={styles.input_field}>
+                    <input type="email" placeholder="Confirme seu E-mail" value={emailConf} onChange={(e) => [setEmailConf(e.target.value), setError("")]}></input>
+                </div>
+                <div className={styles.input_field}>
+                    <input type="password" placeholder="Confirme sua senha" value={senha} onChange={(e) => [setSenha(e.target.value), setError("")]}></input>
+                </div>
+
+
+                    <button onClick={handleSignup}>Cadastrar</button>
+
+                <div className={styles.signup_link}>
+                    <p>Possui uma conta? <Link to='/'>Entrar</Link></p>
+                </div>
+            </form>
+            </div>
+        </section>
+    )
+}
+
+export default Cadastro;
